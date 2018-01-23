dba09d0d13144edd17b1cfc4cf35f1c29066aa2f
Jungwan <baio2033@github.com>
2018-01-23 21:56:35 +0900

    update

--- ip_collector.py
+++ ip_collector.py
@@ -1,26 +1,40 @@
+import threading, time, csv
 import dns.resolver
-import datetime
-import time
+
+def getIP(domain):
+	resolv = dns.resolver.Resolver()
+	ans = resolv.query(domain, "A")
+
+	for r in ans:
+		ip = str(r)
+
+	return ip
+
+def getTime():
+	now = time.localtime()
+	now = "%02d-%02d-%02d %02d:%02d:%02d" %(now.tm_year,now.tm_mon,now.tm_mday,now.tm_hour,now.tm_min,now.tm_sec)
+	return now
+
+def record(domain):
+	while True:
+		f = open('result.csv', 'a')
+		with f:
+			writer = csv.writer(f)
+
+			ip = getIP(domain)
+			date = getTime()
+
+			row = [domain,ip,date]
+			writer.writerow(row)
+			print(row)
+
+		time.sleep(1)
+		f.close()
+
+
 
 if __name__ == "__main__":
+	domain = "fl0ckfl0ck.info"
 
-	#f = open("dns.txt", "w")
-	domain = "xer0ma.info"
-	cnt = 0
-	f = open("result.csv", "w")
-	f.write("idx,time,domain,ip\n")
-	while 1:
-		line = ""
-		resolv = dns.resolver.Resolver()
-		ans = resolv.query(domain, "A")
-
-		now = time.localtime()
-		now = "%02d-%02d-%02d %02d:%02d:%02d" %(now.tm_year,now.tm_mon,now.tm_mday,now.tm_hour,now.tm_min,now.tm_sec)
-		for r in ans:
-			print now, ",", domain, ",",r
-		line += str(cnt) + "," + str(now) + "," + domain + "," + str(r) + "\n"
-		f.write(line)
-		cnt += 1
-		time.sleep(3600)
-
-	f.close()
+	t1 = threading.Thread(target=record, args=(domain,))
+	t1.start()
