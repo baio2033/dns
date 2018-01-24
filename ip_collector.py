42b6b886778bfd10fd6833057841bdfcf815d3f6
Jungwan <baio2033@github.com>
2018-01-25 00:35:34 +0900

    update:

--- ip_collector.py
+++ ip_collector.py
@@ -1,14 +1,14 @@
-import threading, time, csv
+import threading, time, csv, sys
 import dns.resolver
 
-def getIP(domain):
+def getIP(domain,nameserver):
 	resolv = dns.resolver.Resolver()
 
 	try:
 		ans = resolv.query(domain, "A")
 	except:
-		print("[-] dns change -> 8.8.8.8")
-		resolv.nameservers = ['8.8.8.8']
+		print("[-] dns change -> ",nameservers[1])
+		resolv.nameservers = nameserver[1]
 		ans = resolv.query(domain, "A")
 
 	for r in ans:
@@ -21,7 +21,7 @@ def getTime():
 	now = "%02d-%02d-%02d %02d:%02d:%02d" %(now.tm_year,now.tm_mon,now.tm_mday,now.tm_hour,now.tm_min,now.tm_sec)
 	return now
 
-def record(domain):
+def record(domain,nameserver):
 	idx = 0
 	field = ["index","date","domain","IP","DNS"]
 	while True:
@@ -30,7 +30,7 @@ def record(domain):
 			writer = csv.writer(f)
 			if idx == 0:
 				writer.writerow(field)
-			ip, nameserver = getIP(domain)
+			ip, nameserver = getIP(domain,nameserver)
 			date = getTime()
 
 			row = [idx,date,domain,ip,nameserver]
@@ -45,5 +45,27 @@ def record(domain):
 if __name__ == "__main__":
 	domain = "fl0ckfl0ck.info"
 
-	t1 = threading.Thread(target=record, args=(domain,))
+	if len(sys.argv) < 2:
+		nameserver = ["8.8.8.8", "208.67.222.222"]
+	else:
+		nameserver = []
+		nameserver.append(sys.argv[1])
+		nameserver.append(sys.argv[2])
+
+	print('''
+  ___ ____     ____ _               _                 
+ |_ _|  _ \   / ___| |__   ___  ___| | _____ _ __     
+  | || |_) | | |   | '_ \ / _ \/ __| |/ / _ \ '__|    
+  | ||  __/  | |___| | | |  __/ (__|   <  __/ |     _ 
+ |___|_|      \____|_| |_|\___|\___|_|\_\___|_|    (_)
+                                                      
+''')		
+
+	print("\n[+] DNS List\n")
+	for d in nameserver:
+		print("[-] ", d)
+
+	print("\n\n[+] Target Domain\n")
+	print("[-] ", domain, "\n")
+	t1 = threading.Thread(target=record, args=(domain,nameserver))
 	t1.start()
